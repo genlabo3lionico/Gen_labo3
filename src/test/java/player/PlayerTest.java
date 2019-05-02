1c0a3f8225aeeff25a8e30f9255eceaf1d34f9b3
ncdm-stldr <ncdm.stldr@gmail.com>
2019-05-02 21:35:08 +0200

    adding mockito and a simple test using it

--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -5,7 +5,10 @@ import game.Die;
 import game.Piece;
 import game.square.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +24,37 @@ public class PlayerTest {
     @Test
     public void itShouldbePlayer2(){
 
-        assertTrue(p2.getName().equals("Player2"));
+        assertEquals(p2.getName(), ("Player2"));
     }
+
+    //Enables Mock annotations
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void simplePlayerMockTest(){
+        Board board = new Board();
+        Square startingSquare = board.getStartingSquare();
+        Piece piece = new Piece("hat", startingSquare);
+
+        //Create a mock version of die that always return 1
+        Die die = Mockito.mock(Die.class);  //mock version of die
+        Mockito.when(die.getFaceValue()).thenReturn(1); //inject a different behaviour
+
+        int money = 10000;
+
+        Player player = new Player(board, piece, die, money);
+
+        assert(piece.getLocation().getIndex() == 0);
+
+        player.takeTurn();
+
+        assert(piece.getLocation().getIndex() == 1);
+
+    }
+
+
+
 }
