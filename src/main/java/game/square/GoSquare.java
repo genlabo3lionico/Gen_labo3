7b289d8ee6a40e470a32ff936e62fad860bc3974
ncdm-stldr <ncdm.stldr@gmail.com>
2019-05-03 18:54:19 +0200

    factorising common code of Square sub-classes into Square

--- src/main/java/game/square/GoSquare.java
+++ src/main/java/game/square/GoSquare.java
@@ -3,12 +3,6 @@ package game.square;
 public class GoSquare extends Square {
 
     public GoSquare(String name, int index){
-
-        this.name = name;
-
-        if(index > 39){
-            throw new IllegalArgumentException("out of board");
-        }
-        this.index = index;
+        super(name, index);
     }
 }
--- src/main/java/game/square/Square.java
+++ src/main/java/game/square/Square.java
@@ -5,6 +5,14 @@ public abstract class Square {
     protected String name;
     protected int index;
 
+    Square(String name, int index){
+        this.name = name;
+        if(index > 39){
+            throw new IllegalArgumentException("out of board");
+        }
+        this.index = index;
+    }
+
     public int getIndex(){
         return index;
     }
